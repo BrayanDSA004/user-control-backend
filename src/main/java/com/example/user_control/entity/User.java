1d63cf45d7da4ff9aa605a3344d4bf5fb0fc9c41
BrayanDSA004 <142836021+BrayanDSA004@users.noreply.github.com>
2025-12-22 19:12:36 -0500

    feat: create user entity and user repository

---
+++ src/main/java/com/example/user_control/entity/User.java
@@ -0,0 +1,36 @@
+package com.example.user_control.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@SQLRestriction("deleted_at IS NULL")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Role role;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+}
