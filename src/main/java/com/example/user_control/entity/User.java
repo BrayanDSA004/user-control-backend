e8dc7da2a298cd2056ca2fa6ee0b4f9a55d95397
BrayanDSA004 <142836021+BrayanDSA004@users.noreply.github.com>
2026-01-05 12:55:40 -0500

    feat: adapt user entity to spring security userdetails

--- src/main/java/com/example/user_control/entity/User.java
+++ src/main/java/com/example/user_control/entity/User.java
@@ -3,6 +3,12 @@ package com.example.user_control.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.SQLRestriction;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -12,7 +18,7 @@ import org.hibernate.annotations.SQLRestriction;
 @AllArgsConstructor
 @Builder
 @SQLRestriction("deleted_at IS NULL")
-public class User extends BaseEntity {
+public class User extends BaseEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,4 +39,30 @@ public class User extends BaseEntity {
 
     @Column(name = "profile_image")
     private String profileImage;
+
+    // UserDetails
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return getDeletedAt() == null;
+    }
 }
